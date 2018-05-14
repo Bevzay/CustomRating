e310b6faa5562be7168e0caf1fe0ed4a5f3a1be1
Beverly <beverly@Neils-MacBook-Pro.local>
2018-05-15 09:31:40 +1200

    Set rate size

--- CustomRating/CustomRating/RatingControl_StackView.swift
+++ CustomRating/CustomRating/RatingControl_StackView.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 
-class RatingControl_StackView: UIStackView {
+@IBDesignable class RatingControl_StackView: UIStackView {
     // MARK: Properties
     private var ratingButtons = [UIButton]()
     var rating = 0
+    @IBInspectable var starSize: CGSize = CGSize(width: 50.0, height: 50.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
     
     
     // MARK: Initialization
@@ -29,15 +40,21 @@ class RatingControl_StackView: UIStackView {
     // MARK: Private Methods
     private func setupButtons() {
         
-        for _ in 0..<5 {
+        // Clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        
+        for _ in 0..<starCount {
             // Create button
             let button = UIButton()
             button.backgroundColor = UIColor.red
         
             // Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false    // disables the button's automatically generated constraints
-            button.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 50.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
         
             // Setup the button action
             button.addTarget(self, action: #selector(RatingControl_StackView.ratingButtonTapped(button:)), for: .touchUpInside)
